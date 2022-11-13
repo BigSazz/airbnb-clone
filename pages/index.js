7a8760a631a0becf5e5e4d4852ad7468f869d82f
Osaze Oviawe <ocubed88@gmail.com>
2022-11-13 16:25:58 +0100

    feat: - finished home page

--- pages/index.js
+++ pages/index.js
@@ -4,8 +4,15 @@ import Image from 'next/image';
 // components
 import Header from '../app/components/Header';
 import Banner from '../app/components/Banner';
+import SmallCard from '../app/components/SmallCard';
+import MediumCard from '../app/components/MediumCard';
 
-export default function Home() {
+import ExploreData from '../app/lib/exploreData';
+import CardsData from '../app/lib/cardData';
+import LargeCard from '../app/components/LargeCard';
+import Footer from '../app/components/Footer';
+
+export default function Home({ exploreData, cardsData }) {
 	return (
 		<div className=''>
 			<Head>
@@ -18,6 +25,73 @@ export default function Home() {
 
 			{/* Banner Section */}
 			<Banner />
+
+			{/* Main Section */}
+			<main className='max-w-7xl mx-auto px-8 sm:px-16'>
+				<section className='pt-6'>
+					<h2 className='text-4xl font-semibold pb-5'>
+						Explore Nearby
+					</h2>
+
+					{/* Pull some data from a server - API endpoints */}
+					<div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
+						{/* Card */}
+						{exploreData?.map((item) => (
+							<SmallCard
+								key={item.img}
+								img={item.img}
+								location={item.location}
+								distance={item.distance}
+							/>
+						))}
+					</div>
+				</section>
+
+				<section>
+					<h2 className='text-4xl font-semibold py-8'>
+						Live Anywhere
+					</h2>
+
+					<div className='flex space-x-3 overflow-scroll scrollbar-hide p-3 -ml-3'>
+						{cardsData?.map((item) => (
+							<MediumCard
+								key={item.img}
+								img={item.img}
+								title={item.title}
+							/>
+						))}
+					</div>
+				</section>
+
+				<LargeCard
+					img='https://links.papareact.com/4cj'
+					title='The Greatest Outdoors'
+					description='Wishlists curated by Airbnb.'
+					buttonText='Get Inspired'
+				/>
+			</main>
+
+			{/* Footer Section */}
+			<Footer />
 		</div>
 	);
+}
+
+export async function getStaticProps() {
+	// const exploreData = await fetch('https://links.papareact.com/pyp').then(
+	// 	(res) => res.json()
+	// );
+
+	// const cardsData = await fetch('https://links.papareact.com/zp1').then(
+	// 	(res) => res.json()
+	// );
+
+	// console.log('Static============>', ExploreData);
+
+	return {
+		props: {
+			exploreData: ExploreData,
+			cardsData: CardsData,
+		},
+	};
 }
